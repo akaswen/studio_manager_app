d709132f7561aa0f3739176cc1ad45a8581cb5d3
akaswen <aaronofalfheim@gmail.com>
2018-10-30 16:02:49 +0100

    adding in address model and validations

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,16 @@
 class Address < ApplicationRecord
   belongs_to :user
+
+  validates :street_address, format: { with: /\A\d+(\s\w+){2}/ }
+  validates_presence_of :street_address, :city, :state, :zip_code
+  validates :state, length: { is: 2 }
+  validates :zip_code, format: { with: /\A\d{5}\Z/ }
+
+  before_save :upcase_state
+
+  private
+
+  def upcase_state
+    state.upcase!
+  end
 end
--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -8,4 +8,66 @@ RSpec.describe Address, type: :model do
   it('should be valid') do
     expect(@address).to be_valid
   end
+
+  describe('street address') do
+    it('should be present') do
+      @address.street_address = ''
+      expect(@address).to_not be_valid
+    end
+
+    it('should start with a number') do
+      @address.street_address = "hill 55 street"
+      expect(@address).to_not be_valid
+    end
+
+    it('should have both a name and a type of street') do
+      @address.street_address = "55 street"
+      expect(@address).to_not be_valid
+    end
+  end
+
+  it('should have a city') do
+    @address.city = ''
+    expect(@address).to_not be_valid
+  end 
+
+  describe('state') do
+    it('should have a state') do
+      @address.state = ''
+      expect(@address).to_not be_valid
+    end
+
+    it('state should be two letters') do
+      @address.state = 'NJS'
+      expect(@address).to_not be_valid
+      @address.state = 'n'
+      expect(@address).to_not be_valid
+    end
+
+    it('state will be automatically capitalized') do
+      @address.state = 'nj'
+      @address.save
+      @address.reload
+      expect(@address.state).to eq('NJ')
+    end
+  end
+
+  describe('zip code') do
+    it('should be present') do
+      @address.zip_code = ''
+      expect(@address).to_not be_valid
+    end
+
+    it('should be only numbers') do
+      @address.zip_code = "afeew"
+      expect(@address).to_not be_valid
+    end
+
+    it('should be five digits') do
+      @address.zip_code = '555555'
+      expect(@address).to_not be_valid
+      @address.zip_code = '5555'
+      expect(@address).to_not be_valid
+    end
+  end
 end
