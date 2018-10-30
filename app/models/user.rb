2106677aa4fb7649662f8edff34d8a841be68faf
akaswen <aaronofalfheim@gmail.com>
2018-10-30 14:00:50 +0100

    adding in name validations and methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,22 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :confirmable, :lockable
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
+  before_save :lower_case
+
+  def full_name
+    first_name.capitalize + " " + last_name.capitalize
+  end
+
+  private
+
+  def lower_case
+    first_name.downcase!
+    last_name.downcase!
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,4 +8,58 @@ RSpec.describe User, type: :model do
   it('should be valid') do
     expect(@user).to be_valid
   end
+
+  describe('validations') do
+    describe('names') do
+      it('validates presence of first name') do
+        @user.first_name = ''
+        expect(@user).to_not be_valid
+      end
+
+      it('validates presence of last name') do
+        @user.last_name = ''
+        expect(@user).to_not be_valid
+      end
+
+      it('lower cases names') do
+        @user.first_name = 'AaRoN'
+        @user.last_name = 'KaSwEn'
+        @user.save
+        expect(@user.first_name).to eq('aaron')
+        expect(@user.last_name).to eq('kaswen')
+      end
+
+      it('returns a full name') do
+        expect(@user.full_name).to eq('Aaron Kaswen')
+      end
+    end
+
+    describe('email') do
+    end
+
+    describe('password') do
+    end
+
+    describe('associated objects') do
+      describe('addresses') do
+      end
+
+      describe('phone numbers') do
+      end
+    end
+  end
+
+  describe('devise methods') do
+    it('should be resetable') do
+    end
+
+    it('should be rememberable') do
+    end
+
+    it('should be confirmable') do
+    end
+
+    it('should be lockable') do
+    end
+  end
 end
