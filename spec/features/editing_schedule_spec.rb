9ee76f18539884662498ede8c85c23321b8c9fe8
akaswen <aaronofalfheim@gmail.com>
2018-11-13 22:54:34 +0100

    finishing tests for editing schedule

---
+++ spec/features/editing_schedule_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.feature "EditingSchedules", type: :feature do
+  before(:each) do
+    @schedule = create(:schedule)
+    @time_slot = @schedule.time_slots.first
+    @teacher = @schedule.user
+    ActionController::Base.allow_forgery_protection = true
+  end
+
+  it('allows a teacher to change availability of time slots', js: true) do
+    @schedule.time_slots.sunday_slots.each do |s|
+      expect(s.available).to eq(true)
+    end
+    sign_in(@teacher)
+    click_link('Schedule')
+    within('.sunday') do
+      find('.btn-outline-danger').click
+    end
+    find('.fa-calendar-alt').click
+    @schedule.time_slots.sunday_slots.each do |s|
+      expect(s.available).to eq(false)
+    end
+    within('.sunday') do
+      click_button('19:45')
+    end
+    find('.fa-calendar-alt').click
+    expect(@schedule.time_slots.sunday_slots.last.available).to eq(true)
+  end
+end
