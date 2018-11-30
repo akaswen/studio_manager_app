4e36001b5fc14583a9e5d93bffb4f58a9e71de47
akaswen <aaronofalfheim@gmail.com>
2018-11-30 21:37:10 +0100

    adjusting link displays for lesson cards

--- spec/features/dashboard_spec.rb
+++ spec/features/dashboard_spec.rb
@@ -19,7 +19,7 @@ RSpec.feature "Dashboards", type: :feature do
       sign_in(@teacher)
       within('#teacher-lessons') do
         [@lesson, @lesson2].each do |lesson|
-          expect(page).to have_link(lesson.start_time.strftime('%A, %B%e, %I:%M %p'))
+          expect(page).to have_link(href: lesson_path(lesson))
         end
       end
     end
@@ -30,7 +30,7 @@ RSpec.feature "Dashboards", type: :feature do
       sign_in(@student)
       Lesson.where(student_id: @student.id).each do |lesson|
         within('#student-lessons') do
-          expect(page).to have_link(lesson.start_time.strftime('%e, %I:%M %p'))
+          expect(page).to have_link(href: lesson_path(lesson))
        end
      end
     end
@@ -38,23 +38,25 @@ RSpec.feature "Dashboards", type: :feature do
     it('gets only confirmed lessons for a teacher') do
       @lesson2.update_attribute(:confirmed, false)
       sign_in(@teacher)
-      expect(page).to have_link(@lesson.start_time.strftime('%A, %B%e, %I:%M %p'))
-      expect(page).to_not have_link(@lesson2.start_time.strftime('%A, %B%e, %I:%M %p'))
+      within('#teacher-lessons') do
+        expect(page).to have_link(href: lesson_path(@lesson))
+        expect(page).to_not have_link(href: lesson_path(@lesson2))
+      end
     end
 
     it('gets only confirmed lessons for a student') do
       @lesson.update_attribute(:confirmed, false)
       sign_in(@student)
       within('#student-lessons') do
-        expect(page).to_not have_link(@lesson.start_time.strftime('%A, %B%e, %I:%M %p'))
+        expect(page).to_not have_link(href: lesson_path(@lesson))
       end
     end
 
     it('only gets the lessons of a student for that student') do
       sign_in(@student2)
        within('#student-lessons') do
-        expect(page).to_not have_link(@lesson.start_time.strftime('%A, %B%e, %I:%M %p'))
-        expect(page).to have_link(@lesson2.start_time.strftime('%A, %B%e, %I:%M %p'))
+        expect(page).to_not have_link(href: lesson_path(@lesson))
+        expect(page).to have_link(href: lesson_path(@lesson2))
       end
     end
   end
@@ -69,7 +71,7 @@ RSpec.feature "Dashboards", type: :feature do
     it('displays a lesson that is past and unpaid') do
       within('#teacher-sidebar') do
         expect(page).to have_link(@lesson.student.full_name)
-        expect(page).to have_link(@lesson.start_time.strftime('%I:%M %p'))
+        expect(page).to have_link(href: lesson_path(@lesson))
       end
     end
 
@@ -78,7 +80,7 @@ RSpec.feature "Dashboards", type: :feature do
       visit dashboard_path
       within('#teacher-sidebar') do
         expect(page).to_not have_link(@lesson.student.full_name)
-        expect(page).to_not have_link(@lesson.start_time.strftime('%I:%M %p'))
+        expect(page).to_not have_link(href: lesson_path(@lesson))
       end
    end
 
